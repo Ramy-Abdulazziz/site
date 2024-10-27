d64f6fb28320a1893a57cff5c5eb5c5f703da7d6
Ramy <ramy@Kurama.>
2024-10-26 23:24:02 -0400

    feat: updated ease animation for about me

--- components/about.tsx
+++ components/about.tsx
@@ -2,15 +2,35 @@ import * as motion from "framer-motion/client";
 import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { cva } from "class-variance-authority";
+import { Viewport } from "@radix-ui/react-navigation-menu";
+
+const textContainer = {
+    hidden: { opacity: 0 },
+    show: {
+        opacity: 1,
+        transition: {
+            duration: 1,
+            staggerChildren: 0.25,
+        },
+    },
+};
+
+const textItem = {
+    hidden: { opacity: 0, y: 5 },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1,
+            ease: "easeOut",
+        },
+    },
+};
 
 const AboutSection = () => {
     return (
         <motion.div>
             <motion.div
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                viewport={{ once: true }}
                 className={cn(
                     "xl:text-3xl lg:text-3xl md:text-2xl sm:text-xl xs:text-lg font-cairo mb-6"
                 )}
@@ -18,39 +38,46 @@ const AboutSection = () => {
                 Ramy Abdulazziz • رامي عبدالعزيز
             </motion.div>
             <motion.div
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{ duration: 1 }}
+                variants={textContainer}
+                initial="hidden"
+                whileInView="show"
                 viewport={{ once: true }}
-                className={cn(
-                    "font-cairo xl:text-xl lg:text-lg md:text-base sm:text-base xs:text-xs"
-                )}
             >
-                My passion is leveraging technology to solve complex problems
-                and improve people's lives. I recently graduated from Stony
-                Brook University with my B.S. in Computer Science and Electrical
-                Engineering and am curently enrolled in Georgia Tech's online
-                M.S. in Computer Science program.
+                <motion.div
+                    variants={textItem}
+                    className={cn(
+                        "font-cairo xl:text-base lg:text-base md:text-base sm:text-base xs:text-base"
+                    )}
+                >
+                    My passion is leveraging technology to solve complex
+                    problems and improve people's lives. I recently graduated
+                    from Stony Brook University with my B.S. in Computer Science
+                    and Electrical Engineering and am curently enrolled in
+                    Georgia Tech's online M.S. in Computer Science program.
+                </motion.div>
+                <motion.div
+                    variants={textItem}
+                    className={cn(
+                        "justify font-cairo xl:text-base lg:text-base md:text-base sm:text-base xs:text-base mt-6"
+                    )}
+                >
+                    I love digging into the weeds of systems to make
+                    improvements to developer experiences. I enjoy learning
+                    about Operating Systems, Computer Vision, Machine Learning,
+                    and theoretical Computer Science.
+                </motion.div>
+                <motion.div
+                    variants={textItem}
+                    className={cn(
+                        "justify font-cairo xl:text-base lg:text-base md:text-base sm:text-base xs:text-base mt-6"
+                    )}
+                >
+                    I'm based in NY and when I'm not coding, you can find me
+                    enjoying life with my fiance Shazia. Feel free to reach out
+                    with project ideas, new opportunities, or to learn more
+                    about me.
+                </motion.div>
             </motion.div>
-            <motion.div
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                transition={{ duration: 1 }}
-                viewport={{ once: true }}
-                className={cn(
-                    "justify font-cairo xl:text-xl lg:text-lg md:text-base sm:text-sm xs:text-xs mt-6"
-                )}
-            >
-                I love digging into the weeds of systems to make improvements to
-                developer experiences. I enjoy learning about Operating Systems,
-                Computer Vision, Machine Learning, and theoretical Computer
-                Science.
-            </motion.div>
-            <motion.div
-                className={cn(
-                    "justify font-cairo xl:text-xl lg:text-lg md:text-base sm:text-sm xs:text-xs mt-6"
-                )}
-            ></motion.div>
         </motion.div>
     );
 };
---
+++ components/info-section.tsx
@@ -0,0 +1,9 @@
+const InfoSection = () => {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+};
+
+export default InfoSection;
